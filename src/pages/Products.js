30765075f60fdb76503215a0f5730e2dd4325f17
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-10 11:43:34 +0100

    Implement dynamic routes.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
   return (
     <div>
       <h1>The Products Page</h1>
+      <ul>
+        <li><Link to='/products/1'>A Book</Link></li>
+        <li><Link to='/products/2'>A Carpet</Link></li>
+        <li><Link to='/products/3'>A Camera</Link></li>
+      </ul>
     </div>
   )
 }
