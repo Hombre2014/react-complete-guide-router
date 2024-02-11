7db133740a08411669c9985fd06208d9e2950486
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-11 15:13:51 +0100

    Implement upgrade the React router to ver. 6

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
+  const navigate = useNavigate();
+
+  const navigateToProductDetail = (productId) => {
+    navigate(`/products/${productId}`);
+  }
+
   return (
     <div>
       <h1>The Products Page</h1>
       <ul>
-        <li><Link to='/products/1'>A Book</Link></li>
-        <li><Link to='/products/2'>A Carpet</Link></li>
-        <li><Link to='/products/3'>A Camera</Link></li>
+        <li><Link onClick={() => navigateToProductDetail(1)} to='/products/1'>A Book</Link></li>
+        <li><Link onClick={() => navigateToProductDetail(2)} to='/products/2'>A Carpet</Link></li>
+        <li><Link onClick={() => navigateToProductDetail(3)} to='/products/3'>A Camera</Link></li>
       </ul>
     </div>
   )
